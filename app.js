1c98fba6ffef1905b35af0a5eca3272f1b6ca9d6
jrs33 <surettej@bu.edu>
2017-08-11 17:10:12 -0400

    finishing up project page. Will add babel and webpack features next

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// The following section outlines receiving a row of data from a mysql local instance
 var mysql = require('mysql');
 
 const connection = mysql.createConnection({
@@ -18,3 +19,16 @@ connection.query('SELECT * FROM projects_info', (err,rows) => {
   console.log('Data received from Db:\n');
   console.log(rows);
 });
+
+// The following outlines sending data to a local 3000 port
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
